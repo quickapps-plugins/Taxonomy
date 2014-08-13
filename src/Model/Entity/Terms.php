038f2c5f84aa636e10a413562dd3e4a0d4c6cf3a
QuickApps <info@quickapps.es>
2014-08-13 18:57:29 +0200

    update
    making it work with latest cake release

---
+++ src/Model/Entity/Terms.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Licensed under The GPL-3.0 License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @since	 2.0.0
+ * @author	 Christopher Castro <chris@quickapps.es>
+ * @link	 http://www.quickappscms.org
+ * @license	 http://opensource.org/licenses/gpl-3.0.html GPL-3.0 License
+ */
+namespace Taxonomy\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * Represents a single "term" within "terms" table.
+ *
+ */
+class Term extends Entity {
+
+/**
+ * Removes any invalid characters from term's name.
+ * 
+ * @param string $value
+ */
+	public function _setName($value) {
+		$value = strip_tags($value);
+		$value = str_replace(["\n", "\r"], '', $value);
+		return trim($value);
+	}
+
+}
---
+++ src/Model/Entity/Vocabulary.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Licensed under The GPL-3.0 License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @since	 2.0.0
+ * @author	 Christopher Castro <chris@quickapps.es>
+ * @link	 http://www.quickappscms.org
+ * @license	 http://opensource.org/licenses/gpl-3.0.html GPL-3.0 License
+ */
+namespace Taxonomy\Model\Entity;
+
+use Cake\ORM\Entity;
+use Cake\Utility\String;
+
+/**
+ * Represents a single "vocabulary" within "vocabularies" table.
+ *
+ */
+class Vocabulary extends Entity {
+
+/**
+ * Gets a brief description of 80 characters long.
+ * 
+ * @return string
+ */
+	public function _getBriefDescription() {
+		$description = $this->get('description');
+		if (empty($description)) {
+			return '---';
+		}
+		return String::truncate($description, 80);
+	}
+
+/**
+ * Sanitizes vocabulary's description. No HTML allowed.
+ * 
+ * @param string $description
+ * @return string
+ */
+	protected function _setDescription($description) {
+		return strip_tags($description);
+	}
+
+}
